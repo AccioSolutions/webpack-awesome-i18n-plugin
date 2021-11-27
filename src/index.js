1255b0189cac941eb172c2d39f5be4c4a001e036
Matheus Ale <m.matheus.ale@gmail.com>
2021-11-27 11:25:38 -0300

    starts adding unit tests

--- src/index.js
+++ src/index.js
@@ -12,20 +12,19 @@ export default class AwesomeI18NPlugin {
     const pluginName = AwesomeI18NPlugin.name;
     const { webpack } = compiler;
     const { Compilation } = webpack;
+    console.log(compiler.options);
 
     const isWebpack5 = compiler.webpack ? parseInt(compiler.webpack.version, 10) === 5 : false;
 
-    compiler.hooks.thisCompilation.tap(pluginName, compilation => {
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
       compilation.fileDependencies.add(this.options.file);
 
-      const changedTimes = isWebpack5 
-        ? compiler.watchFileSystem.watcher.getTimes() 
-        : (compiler.watchFileSystem.wfs || compiler.watchFileSystem).watcher.mtimes;
+      const changedTimes = isWebpack5 ? compiler.watchFileSystem.watcher.getTimes() : (compiler.watchFileSystem.wfs || compiler.watchFileSystem).watcher.mtimes;
         
       const { startTime = 0 } = compiler.watchFileSystem.watcher;
       if (!changedTimes[this.options.file] || (!!path.extname(this.options.file) && changedTimes[this.options.file] > startTime)) {
 
-        compilation.hooks.processAssets.tap({name: pluginName, stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS}, (assets) => {
+        compilation.hooks.processAssets.tap({ name: pluginName, stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS }, (assets) => {
           Object.entries(this.buildLocales()).forEach(([path, content]) => {
             compilation.emitAsset(path, content)
           })
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,35 @@
-import DemoPlugin from './cjs';
+import { watch } from 'webpack-test-utils';
+import AwesomeI18NPlugin from '.';
 
-test('should set options property', () => {
-  const opts = {
-    test: 'hello',
-  };
-  const plugin = new DemoPlugin(opts);
-  expect(plugin.options).toBe(opts);
+describe('AwesomeI18NPlugin', () => {
+
+  it('shoud', async () => {
+    const volume = {
+      '/src/index.js': 'export default "12345"',
+      '/src/locales/i18n.json': `{
+        "languages": ["pt-BR", "en-US"],
+        "resources": {
+          "header": {
+            "pt-BR": "Inicio",
+            "en-US": "Home"
+          }
+        }
+      }`,
+    };
+
+    const watching = watch(volume, config => {
+      config.plugins.push(
+        new AwesomeI18NPlugin({
+          file: '/src/locales/i18n.json',
+          localesDir: 'locales',
+          genKeysTypes: false,
+        })
+      )
+    });
+    const stats = await watching.build();
+    expect(stats.hasWarnings()).toBe(false);
+    expect(watching.require('/demo/dist/locales/en-US.json')).toBe('');
+
+    await watching.close();
+  });
 });
